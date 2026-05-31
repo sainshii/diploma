d0b8d9e41d147be238d80397d43855f163ad1e38
sainshii <pxlssk00@gmail.com>
2026-05-31 20:56:41 +1000

    full

--- back/project/app/models.py
+++ back/project/app/models.py
@@ -44,7 +44,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='products/')
+    image = models.ImageField(upload_to='products/', max_length=500)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     sizes = models.ManyToManyField(Size, blank=True)
     stock = models.PositiveIntegerField(default=0)
