b5a7a01bc27ce75cc0beabe55cc26500f361769c
sainshii <pxlssk00@gmail.com>
2026-06-25 01:02:40 +1000

    final back???

--- back/project/app/models.py
+++ back/project/app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth import get_user_model
+from django.db.models import Avg
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.conf import settings
 
